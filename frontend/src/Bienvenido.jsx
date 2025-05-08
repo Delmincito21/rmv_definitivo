41f15d3672b38d77eaa0701b7bda14907034ad16
khaterine17 <vasquezcaterin68@gmail.com>
2025-05-08 11:37:14 -0400

    add bienvenido

---
+++ frontend/src/Bienvenido.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Bienvenido.css';
+
+const Bienvenido = () => {
+    return (
+        <div className="bienvenido-container">
+            <h1 className="bienvenido-titulo">¡Bienvenido a nuestra aplicación!</h1>
+            <p className="bienvenido-descripcion">
+                Selecciona tu perfil para continuar:
+            </p>
+            <div className="bienvenido-botones">
+                <button className="btn empleado">Empleado</button>
+                <button className="btn administrador">Administrador</button>
+            </div>
+        </div>
+    );
+};
+
+export default Bienvenido;

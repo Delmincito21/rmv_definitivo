e059921b9b02300ab50e5595adbbc6f06866324f
khaterine17 <vasquezcaterin68@gmail.com>
2025-05-26 22:51:56 -0400

    factura con envio calculado

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Wyro2829",
-    database: "rmvfinal2",
+    password: "caterin170730",
+    database: "AppWeb",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,

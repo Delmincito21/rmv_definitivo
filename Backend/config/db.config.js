6b997e556a019a4d10fa011127f98a0501c17349
khaterine17 <vasquezcaterin68@gmail.com>
2025-06-01 00:33:50 -0400

    a ver

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 // Mostrar todas las variables de entorno disponibles
 console.log('Variables de entorno disponibles:', process.env);
 
+// Configuración de MySQL
 const pool = mysql.createPool({
-    host: 'mysql.railway.internal',
+    host: process.env.MYSQLHOST || 'mysql.railway.internal',
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || 'JEWZIacsisWhxsrEdTrHKjGwEMjvPxKO',
     database: process.env.MYSQLDATABASE || 'railway',
@@ -13,9 +14,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    connectTimeout: 60000,
-    timeout: 60000,
-    connectTimeout: 60000,
+    connectTimeout: 30000,
     acquireTimeout: 60000,
     timeout: 60000,
     debug: true

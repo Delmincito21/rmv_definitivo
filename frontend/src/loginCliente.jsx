8ae1f7573259f3d47e8b472083141f3515e2f21f
khaterine17 <vasquezcaterin68@gmail.com>
2025-06-01 15:19:33 -0400

    Update loginCliente.jsx

--- frontend/src/loginCliente.jsx
+++ frontend/src/loginCliente.jsx
@@ -27,7 +27,7 @@ const LoginCliente = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://backend-production-6925.up.railway.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nombre_usuario: credenciales.usuario, pin_usuario: credenciales.contraseña })
@@ -119,7 +119,7 @@ const LoginCliente = () => {
 
     if (email) {
       try {
-        await fetch('http://localhost:3000/recuperar', {
+        await fetch('https://backend-production-6925.up.railway.app/recuperar', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email })

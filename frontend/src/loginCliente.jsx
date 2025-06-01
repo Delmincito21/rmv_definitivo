5f691b68e578390926131812eb34e30981fd11a5
khaterine17 <vasquezcaterin68@gmail.com>
2025-06-01 02:18:18 -0400

    prueba

--- frontend/src/loginCliente.jsx
+++ frontend/src/loginCliente.jsx
@@ -27,7 +27,7 @@ const LoginCliente = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://rmv-production.up.railway.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nombre_usuario: credenciales.usuario, pin_usuario: credenciales.contraseña })
